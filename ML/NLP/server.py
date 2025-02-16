7d5dbbe25eed893703a62b83b83ab5e31bad21f5
Nikita Zontov <zontovnikita@gmail.com>
2025-02-16 09:10:38 +0300

    HLS

--- ML/NLP/server.py
+++ ML/NLP/server.py
@@ -7,7 +7,7 @@ import wave
 from pydub import AudioSegment
 import websockets
 
-from fastapi import FastAPI, Response, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI, Response
 from fastapi.responses import FileResponse, HTMLResponse
 from config import MODEL_PATH, SAMPLE_RATE
 from fastapi.middleware.cors import CORSMiddleware
@@ -16,9 +16,6 @@ from fastapi.staticfiles import StaticFiles
 from langserve import add_routes
 from agent.agent import graph
 import asyncio
-
-WS_URL = "ws://localhost:8000/ws/transcribe"
-MP3_FILE_PATH = "audio.mp3"
 
 # Путь к модели. Убедитесь, что директория с моделью существует
 
@@ -50,67 +47,23 @@ app.mount('/hls', StaticFiles(directory='hls_content'), name='hls')
 async def test():
     return 'Test is good'
 
-@app.get('/test_client')
-def test_client():
-    asyncio.run(send_audio())
 
-@app.websocket("/ws/transcribe")
-async def transcribe_audio(websocket: WebSocket):
-    """
-    WebSocket-ручка для потокового распознавания речи.
-    Ожидает, что клиент будет слать бинарные аудио-фреймы (bytes).
-    Отдаёт текст: финальный или промежуточный (partial).
-    """
-    await websocket.accept()
+@app.get("/transcribe")
+async def transcribe_audio():
+    wf = wave.open('hls_content/output_000.wav', 'rb')
     rec = vosk.KaldiRecognizer(model, SAMPLE_RATE)
+    results = []
 
-    try:
-        while True:
-            # Получаем очередную порцию аудиоданных
-            data = await websocket.receive_bytes()
-            
-            # Передаём их в распознаватель
-            if rec.AcceptWaveform(data):
-                # Если распознали целую фразу
-                result_text = json.loads(rec.Result())
-                # Отправляем финальный результат
-                await websocket.send_text(result_text["text"] + ' 1')
-            else:
-                # Промежуточные результаты
-                partial_text = json.loads(rec.PartialResult())
-                # Можно отправлять промежуточные варианты
-                await websocket.send_text(partial_text["partial"] + ' 0')
-
-    except Exception as e:
-        print("Ошибка при обработке аудиопотока:", e)
+    while True:          
+        data = wf.readframes(4000)
+        if len(data) == 0:
+            break
+        if rec.AcceptWaveform(data):
+            results.append(rec.Result())
+    results.append(rec.FinalResult())
+    wf.close()
+    parsed_data = [json.loads(item) for item in results]
+    return parsed_data
 
 
-
-def convert_mp3_to_wav(mp3_path, wav_path):
-    audio = AudioSegment.from_mp3(mp3_path)
-    audio = audio.set_channels(1).set_frame_rate(16000)  # Vosk ожидает моно 16kHz
-    audio.export(wav_path, format="wav")
-
-
-async def send_audio():
-    WAV_FILE_PATH = "converted_audio.wav"
-    convert_mp3_to_wav(MP3_FILE_PATH, WAV_FILE_PATH)
-
-    with wave.open(WAV_FILE_PATH, "rb") as wf:
-        async with websockets.connect(WS_URL) as websocket:
-            print("Подключение к WebSocket...")
-            chunk_size = 4000  # Размер фрагмента (примерно 0.25 секунды)
-
-            while True:
-                chunk = wf.readframes(chunk_size)
-                if not chunk:
-                    break  
-
-                await websocket.send(chunk)  
-                response = await websocket.recv()
-                print("Сервер:", response)
-
-            print("Аудио отправлено, закрываем соединение.")
-
-if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=False)
+                
